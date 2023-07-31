d65ea93f8eb1bb21e5e659aa5d2314b86b325755
Pavlo Yerebakan <ppvr3407@gmail.com>
2023-07-31 22:41:51 +0300

    +

---
+++ assets/js/main.js
@@ -0,0 +1,32 @@
+const swiper = new Swiper('.servis-centr', {
+    loop: true,
+    centerInsufficientSlides:true,
+    slidesPerView: 3,
+    spaceBetween: 3,
+    autoplay: {
+        delay: 3000,
+      },
+    speed: 400,
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+  
+
+  });
+  const answer = new Swiper('.reviews', {
+    loop: true,
+    centerInsufficientSlides:true,
+    slidesPerView: 3,
+    spaceBetween: 3,
+    autoplay: {
+        delay: 3000,
+      },
+    speed: 400,
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+  
+
+  });
