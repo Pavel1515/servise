33bb46c5ce7b1109ea5305acf26d9258c5cfc371
Pavlo Yerebakan <ppvr3407@gmail.com>
2023-08-03 16:45:00 +0300

    + mobile

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,32 +1,34 @@
-const swiper = new Swiper('.servis-centr', {
-    loop: true,
-    centerInsufficientSlides:true,
-    slidesPerView: 3,
-    spaceBetween: 3,
-    autoplay: {
-        delay: 3000,
-      },
-    speed: 400,
-    navigation: {
-      nextEl: '.swiper-button-next',
-      prevEl: '.swiper-button-prev',
+const swiper = new Swiper(".servis-centr", {
+  loop: true,
+  centerInsufficientSlides: true,
+  slidesPerView: 3,
+  spaceBetween: 3,
+  autoplay: {
+    delay: 3000,
+  },
+  speed: 400,
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
+const answer = new Swiper(".reviews", {
+  loop: true,
+  centerInsufficientSlides: true,
+  slidesPerView: 1,
+  spaceBetween: 1,
+  autoplay: {
+    delay: 3000,
+  },
+  speed: 400,
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  breakpoints: {
+    921: {
+      slidesPerView: 3,
+      spaceBetween: 3,
     },
-  
-
-  });
-  const answer = new Swiper('.reviews', {
-    loop: true,
-    centerInsufficientSlides:true,
-    slidesPerView: 3,
-    spaceBetween: 3,
-    autoplay: {
-        delay: 3000,
-      },
-    speed: 400,
-    navigation: {
-      nextEl: '.swiper-button-next',
-      prevEl: '.swiper-button-prev',
-    },
-  
-
-  });
+  },
+});
